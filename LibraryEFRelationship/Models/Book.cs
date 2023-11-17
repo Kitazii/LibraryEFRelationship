dc4c2067a0d69c82aac5e290ea9a137b2884c74c
mrkie <mrkie@DESKTOP-04STFUU>
2023-11-17 20:51:58 +0000

    Add project files.

---
+++ LibraryEFRelationship/Models/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LibraryEFRelationship.Models
+{
+    public class Book
+    {
+        [Key]
+        [Display(Name = "ID: ")]
+        public int BookId{ get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+
+        //One Book to Many Genres Relationship
+        [ForeignKey("BookGenre")]
+        public int GenreId { get; set; }
+        public Genre BookGenre { get; set; }
+
+        //Many Books to Many Authors
+        public List<Author> Authors { get; set; }
+    }
+
+    public class Genre
+    {
+        [Key]
+        [Display(Name = "ID: ")]
+
+        public int GenreId { get; set;}
+        public string Name { get; set; }
+
+        //One Book to Many Genres Relationship
+        public List<Book> Books { get; set;}
+    }
+
+    public class Author
+    {
+        [Key]
+        [Display(Name = "ID: ")]
+        public int AuthorId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //Many Books to Many Authors
+        public List<Book> Books { get; set;}
+    }
+}
---
+++ LibraryEFRelationship/Models/LibraryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryEFRelationship.Models
+{
+    public class LibraryDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        public LibraryDbContext() : base("LibraryConnection")
+        {
+            Database.SetInitializer<LibraryDbContext>(new DropCreateDatabaseIfModelChanges<LibraryDbContext>());
+        }
+    }
+}
